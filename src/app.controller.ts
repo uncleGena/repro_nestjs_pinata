e506553cc603c20f82634009cb526bc5c26c671a
Andrew Kudriavtsev <mail.andrew.k@gmail.com>
2021-04-20 14:33:42 +0400

    require pinata with error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pinataSDK = require('@pinata/sdk');
+// const pinata = pinataSDK('yourPinataApiKey', 'yourPinataSecretApiKey');
 
 @Controller()
 export class AppController {
